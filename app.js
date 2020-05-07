8b1d0e87597f5d0866e0b91388d5d8e6b506eb34
Mariam Ali <mariamqali13@gmail.com>
2020-05-07 22:22:27 +0300

    done with feature 2

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.get("/potatoes", (req, res) => {
   res.json(potatoes);
 });
 
+app.get("/potatoes/:potatoId", (req, res) => {
+  const { potatoId } = req.params;
+  const foundPotato = potatoes.find((potato) => potato.id === +potatoId);
+  if (foundPotato) {
+    res.json(foundPotato);
+  } else {
+    res.status(404).json({ message: "Potato not found" });
+  }
+});
+
 app.listen(8000, () => {
   console.log("Running on localhost:8000");
 });
