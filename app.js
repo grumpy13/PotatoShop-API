712010ab680ad9122325523b5fe312a0b093fc97
Mariam Ali <mariamqali13@gmail.com>
2020-05-18 09:14:12 +0300

    done with feature 7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const potatoRoutes = require("./routes/potatoes");
-
 const bodyParser = require("body-parser");
+const db = require("./db/db");
 
 const app = express();
 
@@ -14,6 +14,21 @@ app.get("/", (req, res) => {
 
 app.use("/potatoes", potatoRoutes);
 
-app.listen(8000, () => {
-  console.log("Running on localhost:8000");
-});
+const run = async () => {
+  try {
+    await db.authenticate();
+    console.log("Connection to the database successful!");
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+
+  await app.listen(8000, () => {
+    console.log("The application is running on localhost:8000");
+  });
+};
+
+run();
+
+// app.listen(8000, () => {
+//   console.log("Running on localhost:8000");
+// });
